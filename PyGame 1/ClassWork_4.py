482ee097f7b2dbe95509ce790ab368b83ce324f4
Ha4trueKa4 <kelemandean76@gmail.com>
2023-11-25 15:45:54 +0300

    ClassWork done

---
+++ PyGame 1/ClassWork_4.py
@@ -0,0 +1,31 @@
+import pygame
+
+width, height = 500, 500
+center = width // 2, height // 2
+
+screen = pygame.display.set_mode((width, height))
+pygame.init()
+
+n, k = int(input()), int(input())
+
+
+def draw():
+    screen.fill((0, 0, 0))
+    r = width // 2
+    for i in range(1, k + 2):
+        if i % 3 == 1:
+            color = (0, 0, 255)
+        elif i % 3 == 2:
+            color = (0, 255, 0)
+        else:
+            color = (255, 0, 0)
+        pygame.draw.circle(screen, color, center, r)
+        r -= n
+
+
+draw()
+
+while pygame.event.wait().type != pygame.QUIT:
+    pygame.display.flip()
+
+pygame.quit()
